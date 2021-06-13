427815bc8e1e44e72d0f40b954e9960ff74296a9
destiny boss <ask4menow247@gmail.com>
2021-06-13 00:17:36 -1200

    updated

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ const env = require('dotenv');
 const async = require('async');
 const router = express.Router();
 const {checkUser, checkAdmin, checkSuper} = require('../helpers/auth');
-let paySecret = 'sk_test_57874d9f993d98d4de9658f68b610b79bdad3a31';
+
 env.config({path: '../.env'});
 
 require('../models/User');
@@ -28,7 +28,7 @@ router.get('/user/register', (req, res)=>{
   res.render('users/registration2', {title: 'Sign Up - Eduline'})
 });
 
-router.get('/register', (req, res)=>{
+router.get('/register?', (req, res)=>{
     res.render('users/register', {title: 'Membership Registration - Eduline'});
 });
 
@@ -500,18 +500,6 @@ async.waterfall([
 });
 });
 
-
-router.post("/paystack/pay/verification", function(req, res) {
-    //validate event
-    var hash = crypto.createHmac('sha512', paySecret).update(JSON.stringify(req.body)).digest('hex');
-    if (hash == req.headers['x-paystack-signature']) {
-    // Retrieve the request's body
-    var event = req.body;
-    // Do something with event  
-    event.save();
-    }
-    res.send(200);
-});
 
 
 
