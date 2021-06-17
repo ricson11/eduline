e43723f1b5eb6602edc0a9d0989cd472ffd82924
destiny boss <ask4menow247@gmail.com>
2021-06-16 21:13:00 -1200

    updated

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const path = require('path');
 const env = require('dotenv');
 const {stripTags, adminDate, formatTime, formatDate, formatNew, select, truncate} = require('./helpers/hps');
 require('./config/passport')(passport);
-
+const nodemialer = require('nodemailer');
 env.config({path: './.env'});
-
+require('./models/Contact');
 const app = express();
 
 
@@ -96,6 +96,59 @@ app.use(async function(req, res, next){
     }
     next();
 });
+
+
+
+app.post('/contact', async(req, res)=>{
+    try{
+    const newContact={
+        messageBody: req.body.messageBody,
+        name: req.body.name,
+         email: req.body.email,
+      
+     }
+     contact = await Contact.create(newContact)
+     console.log(contact)
+    
+     let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        tls:{
+            rejectUnauthorized: false,
+        },
+        auth:{
+         user: process.env.GMAIL_EMAIL,
+         pass:process.env.GMAIL_PASS
+        },
+    });
+
+     var mailOptions={
+                   from: req.body.email,
+                   to: process.env.GMAIL_EMAIL,
+                   replyTo: req.body.email,
+                   subject: 'Message from' +' '+ req.body.name,
+                   text: req.body.messageBody,
+          };
+       
+         transporter.sendMail(mailOptions, function(err, info){
+           if(err){
+               console.log(err)
+               req.flash('error_msg', 'Message not sent, try again')
+               return res.redirect('back')
+           }else{
+               console.log('Message sent successfully' + info.response)
+               req.flash('success_msg', 'Message delivered successfully')
+               return res.redirect('back')
+           }
+       })
+    }
+    catch(err){
+        console.log(err.message)
+        res.redirect('/500');
+    }
+});
+
 
 
 //routes
--- routes/contact.js
+++ routes/contact.js
@@ -12,57 +12,6 @@ require('../models/User');
 
 
 
-router.post('/contact', async(req, res)=>{
-    try{
-    const newContact={
-        messageBody: req.body.messageBody,
-        name: req.body.name,
-         email: req.body.email,
-      
-     }
-     contact = await Contact.create(newContact)
-     console.log(contact)
-    
-     let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        tls:{
-            rejectUnauthorized: false,
-        },
-        auth:{
-         user: process.env.GMAIL_EMAIL,
-         pass:process.env.GMAIL_PASS
-        },
-    });
-
-     var mailOptions={
-                   from: req.body.email,
-                   to: process.env.GMAIL_EMAIL,
-                   replyTo: req.body.email,
-                   subject: 'Message from' +' '+ req.body.name,
-                   text: req.body.messageBody,
-          };
-       
-         transporter.sendMail(mailOptions, function(err, info){
-           if(err){
-               console.log(err)
-               req.flash('error_msg', 'Message not sent, try again')
-               return res.redirect('back')
-           }else{
-               console.log('Message sent successfully' + info.response)
-               req.flash('success_msg', 'Message delivered successfully')
-               return res.redirect('back')
-           }
-       })
-    }
-    catch(err){
-        console.log(err.message)
-        res.redirect('/500');
-    }
-});
-
-
 
 router.get('/contact/:slug', async(req, res)=>{
     try{
