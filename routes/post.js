6465ae74ef1c04258fd0f796ce00651bf874eb83
destiny boss <ask4menow247@gmail.com>
2021-06-17 15:00:16 -1200

    updated

--- routes/post.js
+++ routes/post.js
@@ -16,8 +16,7 @@ router.get('/',  async(req, res)=>{
     let post1 = await Post.find({status: 'published'}).sort({updatedAt:-1, date:-1}).limit(1);
     let post2 = await Post.find({status: 'published'}).sort({updatedAt:-1, date:-1}).skip(1).limit(1);
     let post3 = await Post.find({status: 'published'}).sort({updatedAt:-1, date:-1}).skip(2).limit(1);
-     let post = await Post.findOne({slug: req.params.slug});
-    res.render('index', { title: `${post.slug} - Eduline` , post1, post2, post3});
+    res.render('index', { title: title , post1, post2, post3});
 });
 
 //Saved posts
